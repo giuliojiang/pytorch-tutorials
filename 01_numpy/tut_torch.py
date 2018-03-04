22cd7282741f773f09aed921ba892acd69b4c7b5
giuliojiang <giuliojiang@gmail.com>
2018-03-04 15:24:08 +0000

    Do tutorial on numpy and torch manual gradient descent

---
+++ 01_numpy/tut_torch.py
@@ -0,0 +1,40 @@
+import torch
+
+# dtype = torch.FloatTensor
+dtype = torch.cuda.FloatTensor
+
+N = 64      # batch size
+D_in = 1000 # input dimension
+H = 100     # hidden dimension
+D_out = 10  # output dimension
+
+# Create random input and output data
+x = torch.randn(N, D_in).type(dtype)
+y = torch.randn(N, D_out).type(dtype)
+
+# Randomly initialize weights
+w1 = torch.randn(D_in, H).type(dtype)
+w2 = torch.randn(H, D_out).type(dtype)
+
+learning_rate = 1e-6
+for t in range(500):
+    # Forward pass: compute predicted y
+    h = x.mm(w1)
+    h_relu = h.clamp(min=0)
+    y_pred = h_relu.mm(w2)
+
+    # Compute and print loss
+    loss = (y_pred - y).pow(2).sum()
+    print(t, loss)
+
+    # Compute gradients
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h<0] = 0
+    grad_w1 = x.t().mm(grad_h)
+
+    # Upate weights using gradient descent
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
